ff7f33481b71a25014a19b895443c7d167c170a1
Khizar Aziz <khizar.aziz@gmail.com>
2017-12-12 16:48:21 +0100

    Routing added with Dashboard and Heroes routes. Also styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { HeroDetailComponent } from "./hero-detail.component";
 
+import { HeroDetailComponent } from "./hero-detail.component";
 import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./heroes.component";
+import { HeroService } from "./hero.service";
+import { DashboardComponent } from "./dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
 export class AppModule {}
