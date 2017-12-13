10bc1d1f79220dcd7a36ae1b7cb20657ff0c04ae
Khizar Aziz <khizar.aziz@gmail.com>
2017-12-13 16:54:34 +0100

    added add, delete, edit and search for heroes. also added http support using in memory web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { HeroDetailComponent } from "./hero-detail.component";
 import { AppComponent } from "./app.component";
@@ -8,10 +9,16 @@ import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "./hero.service";
 import { DashboardComponent } from "./dashboard.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { HeroSearchComponent } from "./hero-search.component";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService)],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent, HeroSearchComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
 })
